1d63b668aec4a4e2fd6aa11607d6de2a9b61de0b
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-06-24 22:43:45 +0200

    Read `API_URL` from env

--- client/internal/client/client.go
+++ client/internal/client/client.go
@@ -1,15 +1,33 @@
 package internal
 
 import (
+	"log"
 	"net/http"
+	"net/url"
+	"os"
 )
 
 type Client struct {
+	ApiUrl     *url.URL
 	HttpClient http.Client
 }
 
 func NewClient() *Client {
 	return &Client{
+		ApiUrl:     getApiUrlFromEnv(),
 		HttpClient: http.Client{}, // TODO: think about params that may be needed in prod
 	}
+}
+
+func getApiUrlFromEnv() *url.URL {
+	const API_URL_ENV_VAR_NAME = "API_URL"
+
+	urlFromEnv := os.Getenv(API_URL_ENV_VAR_NAME)
+	apiUrl, err := url.Parse(urlFromEnv)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return apiUrl
 }
