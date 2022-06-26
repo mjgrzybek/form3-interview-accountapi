6755c327528edd0b1f06fbfe44f70e565f9fc004
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-06-26 18:10:50 +0200

    Use docker-compose for test image building

---
+++ client/build/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# based on https://docs.docker.com/compose/startup-order/
+
+set -e
+
+readonly SVC_NAME=accountapi
+
+waitForSvc() {
+  until curl -X GET "http://${1}:8080/v1/health" --connect-timeout 1 &> /dev/null; do
+    echo "${SVC_NAME} service is not yet available - sleeping"
+    sleep 1
+  done
+}
+
+waitForSvc "${SVC_NAME}"
+echo "${SVC_NAME} service is up - executing tests"
+
+go test -tags e2e ./...
